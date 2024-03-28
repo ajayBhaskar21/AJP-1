12b36d28a36515c519b3918f495fbb1834cbd473
ajayBhaskar21 <ajaybhaskar2933@gmail.com>
2024-03-28 11:25:40 +0530

    spring ipl teams

---
+++ iplAnnotation/src/main/java/com/example/iplAnnotation/MainApp.java
@@ -0,0 +1,22 @@
+package com.example.iplAnnotation;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.example")
+public class MainApp {
+    public static void main(String[] args) {
+        // Load the Spring context
+    	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainApp.class);
+
+        // Retrieve the bean from the Spring context
+        TeamService teamService = (TeamService) context.getBean("teamService");
+
+        teamService.runMenu();
+    }
+}
+
